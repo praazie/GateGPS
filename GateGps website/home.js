a9c323bdff6efa687219de59a1b73626c764f61e
praazie <officepraise01@gmail.com>
2025-06-17 00:37:14 +0100

    Add files via upload

---
+++ GateGps website/home.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const heroCarousel = document.querySelector('#heroCarousel');
+  new bootstrap.Carousel(heroCarousel, {
+    interval: 3000,
+    ride: 'carousel',
+    pause: false,
+    touch: true
+  });
+});
+
+
