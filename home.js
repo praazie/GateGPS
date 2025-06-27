4356b56aee8cc5eae0b11b6408a8e4bdf4e7d496
praazie <officepraise01@gmail.com>
2025-06-27 14:25:02 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const heroCarousel = document.querySelector('#heroCarousel');
+  new bootstrap.Carousel(heroCarousel, {
+    interval: 3000,
+    ride: 'carousel',
+    pause: false,
+    touch: true
+  });
+});
+
+
+
+const viewMoreBtn = document.querySelector('.view-more-btn');
+const moreInstallers = document.querySelector('#moreInstallers');
+
+viewMoreBtn.addEventListener('click', function () {
+  const isHidden = moreInstallers.classList.contains('d-none');
+
+  if (isHidden) {
+    moreInstallers.classList.remove('d-none');
+    moreInstallers.classList.add('animate__animated', 'animate__fadeIn');
+    viewMoreBtn.textContent = 'View Less';
+  } else {
+    moreInstallers.classList.add('d-none');
+    viewMoreBtn.textContent = 'View More';
+  }
+});
